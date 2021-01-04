d966d8b335aff5d6362555143223e9ac869846ab
Mert FINDICAK <mfindicak@cits.com.tr>
2021-01-04 22:31:38 +0300

    Added 2D and can change between 2D to 3D.

--- Source.cpp
+++ Source.cpp
@@ -11,6 +11,7 @@ std::list<std::string> names;
 std::list<int> numbers;
 char title[] = "Data Visualization 031890002";
 int refreshMills = 15;
+int graph = 2;
 
 Camera camera;
 
@@ -82,6 +83,12 @@ void drawCube(GLdouble height) {
 }
 
 void draw3D(std::list<std::string> listNames, std::list<int> listNumbers) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
+	glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+
+	glLoadIdentity();                 // Reset the model-view matrix
+	gluLookAt(camera.x, camera.y + 2, camera.z, 0, 2, 0, 0, 1, 0);
+
 	int listSize = listNumbers.size();
 	int counter = 0;
 	for (int x : listNumbers) {
@@ -97,10 +104,63 @@ void draw3D(std::list<std::string> listNames, std::list<int> listNumbers) {
 	}
 }
 
+void draw2D(std::list<std::string> listNames, std::list<int> listNumbers) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	gluOrtho2D(0, 640, 0, 480);
+	GLsizei xRaster = 25, yRaster = 150;
+	GLubyte etiket[36] = { 'O','c','a',	'S','u','b',	'M','a','r',
+						'N','i','s',	'M','a','y',	'H','a','z',
+						'T','e','m',	'A','g','u',	'E','y','l',
+						'E','k','m',	'K','a','s',	'A','r','a', };
+	GLint dataValue[12] = { 420, 342, 324, 310, 262, 185,
+							190, 196, 217, 240, 312, 438 };
+
+	GLint ay, k;
+	GLint x = 30; //grafik için x konumunu ilklendir
+
+	glClear(GL_COLOR_BUFFER_BIT); //Gösterim penceresini temizle
+	glLineWidth(3.0);			//Çizgi kalýnlýðýný ayarla
+	glEnable(GL_LINE_STIPPLE);	//Çizgi özellik deðiþimini aktifleþtir
+	glColor3f(0.0f, 0.0f, 1.0f);			//çizgi rengini maviye ata
+	//Veriyi kýrýklý hat þeklinde çizdir
+	glBegin(GL_LINE_STRIP);
+	for (k = 0; k < 12; k++)
+		glVertex2i(x + k * 50, dataValue[k]);
+	glEnd();
+	glDisable(GL_LINE_STIPPLE);		//Çizgi özellik deðiþimini inaktifleþtir
+
+	//Veri noktalarýný yýldýz þeklinde çizdir
+	glColor3f(1.0f, 0.0f, 0.0f);			//iþaretçi rengini kýrmýzýya ata
+	for (k = 0; k < 12; k++) {
+		glRasterPos2i(xRaster + k * 50, dataValue[k] - 4);
+		glutBitmapCharacter(GLUT_BITMAP_9_BY_15, '*');
+	}
+
+	//Grafik etiketlerini çizdir
+	glColor3f(0.0f, 0.0f, 0.0f);			//yazý rengini siyaha ata
+	xRaster = 20;
+	for (ay = 0; ay < 12; ay++) {
+		glRasterPos2i(xRaster, yRaster);
+		for (k = 3 * ay; k < 3 * ay + 3; k++)
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, etiket[k]);
+		xRaster += 50;
+	}
+}
+
+void init2D() {
+	glLoadIdentity();
+	glClearColor(1.0, 1.0, 1.0, 0.0);		// beyaz renkte gösterim penceresi
+	glMatrixMode(GL_PROJECTION);
+	glDisable(GL_DEPTH_TEST);
+}
+
 void initGL() {
+	glLoadIdentity();
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
 	glClearDepth(1.0f);                   // Set background depth to farthest
 	glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
+	gluPerspective(45.0f, (double)640 / 480, 0.1f, 100.0f);
 	glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
 	glShadeModel(GL_SMOOTH);   // Enable smooth shading
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
@@ -109,13 +169,8 @@ void initGL() {
 /* Handler for window-repaint event. Called back when the window first appears and
    whenever the window needs to be re-painted. */
 void display() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
-	glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
-
-	glLoadIdentity();                 // Reset the model-view matrix
-	gluLookAt(camera.x, camera.y + 2, camera.z, 0, 2, 0, 0, 1, 0);
-
-	draw3D(names, numbers);
+	if (graph == 1) draw3D(names, numbers);
+	else if (graph == 2) draw2D(names, numbers);
 
 	glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
 }
@@ -147,22 +202,29 @@ void keyboard(unsigned char key, int x, int y) {
 	if (key == 'a' or key == 'A') {
 		camera.turnLeft(3);
 	}
-	if (key == 'd' or key == 'D') {
+	else if (key == 'd' or key == 'D') {
 		camera.turnRight(3);
 	}
-	if (key == 'w' or key == 'W') {
+	else if (key == 'w' or key == 'W') {
 		camera.changeDistance(-0.2);
 	}
-	if (key == 's' or key == 'S') {
+	else if (key == 's' or key == 'S') {
 		camera.changeDistance(0.2);
 	}
-	if (key == 'e' or key == 'E') {
+	else if (key == 'e' or key == 'E') {
 		camera.y += 0.7f;
 	}
-	if (key == 'f' or key == 'F') {
+	else if (key == 'f' or key == 'F') {
 		camera.y -= 0.7f;
 	}
-
+	else if (key == '1') {
+		initGL();
+		graph = 1;
+	}
+	else if (key == '2') {
+		init2D();
+		graph = 2;
+	}
 }
 
 /* Main function: GLUT runs as a console application starting at main() */
@@ -175,7 +237,8 @@ int main(int argc, char** argv) {
 	glutCreateWindow(title);          // Create window with the given title
 	glutDisplayFunc(display);       // Register callback handler for window re-paint event
 	glutReshapeFunc(reshape);       // Register callback handler for window re-size event
-	initGL();                       // Our own OpenGL initialization
+	if (graph == 1) initGL();		// Our own OpenGL initialization
+	else if (graph == 2) init2D();
 	glutKeyboardFunc(keyboard);
 	glutTimerFunc(0, timer, 0);     // First timer call immediately [NEW]
 	glutMainLoop();                 // Enter the infinite event-processing loop
